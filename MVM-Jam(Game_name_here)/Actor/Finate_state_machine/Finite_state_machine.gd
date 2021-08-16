0e2aa0687016275a3c2dfd3ceb168a33f3024a21
Madhava Prasath <69044879+Madhavaprasath@users.noreply.github.com>
2021-08-16 10:18:30 +0530

    Statemachine intial stage

---
+++ MVM-Jam(Game_name_here)/Actor/Finate_state_machine/Finite_state_machine.gd
@@ -0,0 +1,33 @@
+extends Node
+class_name Finite_state_machine
+
+
+var previous_state
+var current_state
+var stack=[]
+
+
+onready var actor=get_parent()
+
+
+func _physics_process(delta):
+    current_state=get_currentstate()
+    if current_state!=null:
+        var state=get_node(current_state)
+        state.play_state(delta)
+        var next_state=state.check_exit_condition(delta)
+        if next_state!=null:
+            previous_state=pop_state()
+            push_state(next_state)
+        
+
+func pop_state():
+    return stack.pop()
+
+func push_state(state):
+    stack.push(state)
+
+
+
+func get_currentstate():
+    return stack[len(stack)-1] if len(stack)>0 else null
