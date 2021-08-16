fa7f48e9e5a2bde669bc292d594abf9627296a18
Madhava Prasath <69044879+Madhavaprasath@users.noreply.github.com>
2021-08-16 12:58:27 +0530

    Added state

--- MVM-Jam(Game_name_here)/Actor/Finate_state_machine/Finite_state_machine.gd
+++ MVM-Jam(Game_name_here)/Actor/Finate_state_machine/Finite_state_machine.gd
@@ -11,23 +11,23 @@ onready var actor=get_parent()
 
 
 func _physics_process(delta):
-    current_state=get_currentstate()
-    if current_state!=null:
-        var state=get_node(current_state)
-        state.play_state(delta)
-        var next_state=state.check_exit_condition(delta)
-        if next_state!=null:
-            previous_state=pop_state()
-            push_state(next_state)
-        
+	current_state=get_currentstate()
+	if current_state!=null:
+		var state=get_node(current_state)
+		state.play_state(delta)
+		var next_state=state.check_exit_condition(delta)
+		if next_state!=null:
+			previous_state=pop_state()
+			push_state(next_state)
+		
 
 func pop_state():
-    return stack.pop()
+	return stack.pop()
 
 func push_state(state):
-    stack.push(state)
+	stack.push(state)
 
 
 
 func get_currentstate():
-    return stack[len(stack)-1] if len(stack)>0 else null
+	return stack[len(stack)-1] if len(stack)>0 else null
---
+++ MVM-Jam(Game_name_here)/Actor/Finate_state_machine/State.gd
@@ -0,0 +1,10 @@
+extends Node
+class_name state
+
+
+
+func play_state(delta):
+	pass
+
+func  check_exit_condition(delta):
+	pass
